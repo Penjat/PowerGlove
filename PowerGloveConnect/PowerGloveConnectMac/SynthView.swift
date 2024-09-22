22460998df17372515d8733ce82c39c8f8089809
penjat <ssymington@lululemon.com>
2024-09-22 09:12:03 -0700

    Updated.

---
+++ PowerGloveConnect/PowerGloveConnectMac/SynthView.swift
@@ -0,0 +1,18 @@
+//
+//  SynthView.swift
+//  PowerGloveConnectMac
+//
+//  Created by Spencer Symington on 2024-09-21.
+//
+
+import SwiftUI
+
+struct SynthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SynthView()
+}
